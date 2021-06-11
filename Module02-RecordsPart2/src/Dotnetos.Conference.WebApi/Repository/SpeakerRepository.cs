555260f6a6e031c040040cb3db213adf4d5a15ae
Grégoire Wulliamoz <gwulliamoz@greenmotion.ch>
2021-06-11 15:25:37 +0200

    solution module2

--- Module02-RecordsPart2/src/Dotnetos.Conference.WebApi/Repository/SpeakerRepository.cs
+++ Module02-RecordsPart2/src/Dotnetos.Conference.WebApi/Repository/SpeakerRepository.cs
@@ -6,7 +6,8 @@ namespace Dotnetos.Conference.WebApi.Repository
     public class SpeakerRepository
     {
         // not exactly true, fortunately in 2018 we had all speakers on the stage :)
-        private readonly Speaker[] _speakers2018 = {
+        private readonly Speaker[] _speakers2018 =
+        {
             new("Adam", "Sitnik", "Microsoft"),
             new("Alexandre", "Mutel", "Unity Technologies"),
             new OnlineSpeaker("Andrey", "Akinshin", "JetBrains", true),
@@ -14,7 +15,7 @@ namespace Dotnetos.Conference.WebApi.Repository
             new("Jarosław", "Pałka", "Neo4j"),
             new OnlineSpeaker("Łukasz", "Pyrzyk", "Dotnetos", false),
             new OnlineSpeaker("Konrad", "Kokosa", "Dotnetos", false),
-            new OnlineSpeaker("Szymon", "Kulec", "Dotnetos", true),
+            new OnlineSpeaker("Szymon", "Kulec", "Dotnetos", true)
         };
 
         public Speaker[] GetAll() => _speakers2018.ToArray();
