1a9891754b7e657295a1f785bc5f4d407412dae9
Grégoire Wulliamoz <gwulliamoz@greenmotion.ch>
2021-06-11 16:04:21 +0200

    solution module3

--- Module03-TopLevelStatements/WhenWasBorn/Program.cs
+++ Module03-TopLevelStatements/WhenWasBorn/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 
@@ -6,13 +8,14 @@ using System.Net.Http.Json;
 
 if (args.Length != 1)
 {
-    // TODO: define local PrintHelp function to print help and exit
+    PrintHelp();
+    return;
 }
 
 // TODO: utilize https://swapi.dev/ to search for a person with a given name
 //       Hint: Use https://swapi.dev/documentation and look for "Searching"
 using HttpClient client = new HttpClient();
-var requestUri = "?";
+var requestUri = $"https://swapi.dev/api/people/?search={args[0]}";
 var response = await client.GetFromJsonAsync<PersonsDTO>(requestUri);
 
 if (response?.Count != 1)
@@ -21,10 +24,17 @@ if (response?.Count != 1)
 }
 else
 {
-    // TODO: 
+    var person = response.Results.First();
     Console.WriteLine($"{person.Name} was born {person.Birth_Year}.");
 }
 
 // TODO: define PersonDTO and PersonsDTO records to deserialize (only necessary)
 //       fields from https://swapi.dev/api/people/?search=... results.
 
+void PrintHelp()
+{
+    Console.WriteLine("Print help");
+}
+
+record PersonsDTO(int Count, IEnumerable<PersonDTO> Results);
+record PersonDTO(string Name, string Birth_Year);
