9ea885cf278453745088e9dfc7c2d0c6d54fe059
Grégoire Wulliamoz <gwulliamoz@greenmotion.ch>
2021-07-02 15:05:20 +0200

    solution

--- Module04-PatternMatching/Exercises/GameEngine/GameService.cs
+++ Module04-PatternMatching/Exercises/GameEngine/GameService.cs
@@ -4,8 +4,8 @@ namespace GameEngine
 {
     public class GameService
     {
-        private Player _currentMovePlayer;
         public const int MaxMoves = 128;
+        private readonly Player _currentMovePlayer;
 
         public GameService(Player currentMovePlayer)
         {
@@ -15,15 +15,13 @@ namespace GameEngine
         // TODO: Brain exercise :) Without changing Process method signature, change
         // ProcessCommand to use pattern matching and as little ifs as possible 
         // (or none at all, the best solution!)
-        public bool ProcessCommand(CommandDTO command) {
-            if (command is null)
-                throw new ArgumentNullException(nameof(command));
-            if (command.PlayerSide != _currentMovePlayer)
-                throw new InvalidMoveException("Invalid player side");
-            if (command.Index > MaxMoves || command.Index < 0)
-                throw new InvalidMoveException("Invalid game index");
-            return Process(command.PlayerSide, (uint)command.Index, command.Move);
-        }
+        public bool ProcessCommand(CommandDTO command) => command switch
+        {
+            null => throw new ArgumentNullException(nameof(command)),
+            var (_, p, _) when p != _currentMovePlayer => throw new InvalidMoveException("Invalid player side"),
+            { Index: > MaxMoves or < 0 } => throw new InvalidMoveException("Invalid game index"),
+            _ => Process(command.PlayerSide, (uint)command.Index, command.Move)
+        };
 
         private bool Process(Player player, uint index, MoveType move)
         {
